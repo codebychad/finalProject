8173f5f504ba99ca943572f9181436aee4ff4227
codebychad <codebychad@gmail.com>
2023-08-22 13:33:06 -0500

    day2 commit2

---
+++ src/main/java/com/project/schoolmanagment/controller/user/AdminController.java
@@ -0,0 +1,22 @@
+package com.project.schoolmanagment.controller.user;
+
+import com.project.schoolmanagment.service.user.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+	private final AdminService adminService;
+
+	@PostMapping("/save")
+	public ResponseEntity<>saveAdmin(){
+		return null;
+	}
+
+}
---
+++ src/main/java/com/project/schoolmanagment/repository/user/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.project.schoolmanagment.repository.user;
+
+import com.project.schoolmanagment.entity.concretes.user.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+}
